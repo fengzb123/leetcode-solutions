b5381c5d9424620a1779deec64b3d30f19734d97
fengzb123 <lty2003115@gmail.com>
2026-07-25 10:09:31 +0800

    补充

--- 0283-moveZeroes/Solution.java
+++ 0283-moveZeroes/Solution.java
@@ -1,5 +1,23 @@
+/**
+ * 0283. 移动0
+ * https://leetcode.cn/problems/move-zeroes
+ *
+ * 思路：使用快慢指针来遍历数组，查找0元素，每当快指针找到一个非0元素，就交换快慢指针
+ *
+ *
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
+ */
 class Solution {
     public void moveZeroes(int[] nums) {
-        
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != 0) {
+                int temp = nums[slow];
+                nums[slow] = nums [fast];
+                nums[fast] = temp;
+                slow++;
+            }
+        }
     }
 }
